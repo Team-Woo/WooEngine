d496135420f51c5c464b69b7ba04dc13a2c4d34d
Zach <zachburke859@gmail.com>
2020-10-23 13:37:17 -0400

    events, interactivity! we did it fam

--- WooEngine/src/WooEngine/ImGui/ImGuiLayer.h
+++ WooEngine/src/WooEngine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "WooEngine\Layer.h"
 
+#include "WooEngine\Events\KeyEvent.h"
+#include "WooEngine\Events\MouseEvent.h"
+#include "WooEngine\Events\ApplicationEvent.h"
+
 namespace WE {
 
 	class WOOENGINE_API ImGuiLayer : public Layer
@@ -16,6 +20,21 @@ namespace WE {
 		virtual void OnUpdate();
 		virtual void OnEvent(Event& event);
 	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
+
+
+	private:
+
 		float m_Time = 0.0f;
 	};
 
